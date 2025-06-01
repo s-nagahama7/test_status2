d582e07d40b736b501174209d14b21b7d90675d4
test <s.nagahama88@gmail.com>
2025-06-01 18:47:46 +0900

    202506011847

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const production = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: production ? '/' : '',
+  assetPrefix: production ? '/test_status2/' : '',
   reactStrictMode: true,
   swcMinify: true,
 }
